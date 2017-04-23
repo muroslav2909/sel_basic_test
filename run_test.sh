fb083243fddce857dd4a2d4588651186b186102d
muroslav2909 <myroslav@preply.com>
2017-04-23 20:05:24 +0300

    eff

--- run_test.sh
+++ run_test.sh
@@ -1,13 +1,6 @@
 #!/bin/sh
 sudo pip install -U pytest
 echo "Start run selenium tests for"
-
-if [ $BRANCH_NAME = master ]; then
-    export BRANCH_NAME=$TRAVIS_PULL_REQUEST_BRANCH;
-    echo $BRANCH_NAME "->master";
-else
-    echo $TRAVIS_PULL_REQUEST_BRANCH "->" $BRANCH_NAME;
-fi
-
+echo $BRANCH_NAME
 export PATH=$PATH:$(pwd)/drivers
 pytest
